204864968b66490be73d819abaf2b2db74b5cf33
NeverZero <laguirre314@gmail.com>
2022-07-21 00:02:27 -0600

    Start of nutrition label project.

---
+++ ingredients.py
@@ -0,0 +1,19 @@
+ap_flour = {'unit': '100_g', 'calories': 364, 'fat': 1, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 2, 'carbs': 76, 'fiber': 2.7, 'sugars': 0.3, 'added_sugar': 0, 'protein': 100}
+sugar_gran = {'unit': '100_g', 'calories': 387, 'fat': 0, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 1, 'carbs': 100, 'fiber': 0, 'sugars': 100, 'added_sugar': 100, 'protein': 0}
+sugar_brown = {'unit': '100_g','calories': 380, 'fat': 0, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 28, 'carbs': 98, 'fiber': 0, 'sugars': 97, 'added_sugar': 100, 'protein': 0.001}
+butter_unsalted = {'unit': '100_g', 'calories': 717, 'fat': 1, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 643, 'carbs': 0.1, 'fiber': 0, 'sugars': 0.1, 'added_sugar': 0, 'protein': 0.8}
+egg_large = {'unit': 'each', 'calories': 78, 'fat': 5, 'sat_fat': 1.6, 'trans_fat': None, 'cholesterol': 187, 'sodium': 62, 'carbs': 0.6, 'fiber': 0, 'sugars': 0.6, 'added_sugar': 0, 'protein': 6}
+walnut = {'unit': '100_g', 'calories': 654, 'fat': 65, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 6, 'carbs': 14, 'fiber': 6.7, 'sugars': 2.6, 'added_sugar': 0, 'protein': 15}
+oats_rolled = {'unit': '100_g', 'calories': 379, 'fat': 6.5, 'sat_fat': 0, 'trans_fat': 0, 'cholesterol': 0, 'sodium': 0, 'carbs': 68, 'fiber': 10, 'sugars': 1, 'added_sugar': 0, 'protein': 13.2}
+flour_whole = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol': None, 'sodium': None, 'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein': None}
+flour_cake = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol':                 None, 'sodium': None, 'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein': None}
+sucanat = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol': None, 'sodium': None, 'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein': None}
+cocoa = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol': None, 'sodium': None,'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein'         : None}
+choc_chip_semisweet = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol': None, 'sodium': None,     'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein': None}
+salt = {'unit': '100_g', 'calories': None, 'fat': None, 'sat_fat': None, 'trans_fat': None, 'cholesterol': None, 'sodium': 10000, 'carbs': None, 'fiber': None, 'sugars': None, 'added_sugar': None, 'protein': None}
+
+print (f"Calories for ap_flour= {ap_flour['calories']}")
+print (f"Egg unit= {egg_large['unit']}")
+ap_flour['carbs'] = 20
+ap_flour
+print(ap_flour)
